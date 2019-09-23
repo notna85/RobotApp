c84d8e76514effbb2735c497516cf02b362af488
Anton John Erik Sundström <sundstrom85@gmail.com>
2019-09-23 14:52:20 +0200

    Implemented onclick method to connect to device

--- app/src/main/java/com/example/boldi/bluetoothcarcontroller/BlueTooth.java
+++ app/src/main/java/com/example/boldi/bluetoothcarcontroller/BlueTooth.java
@@ -8,13 +8,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.fragment.app.Fragment;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -24,16 +22,15 @@ import java.util.UUID;
 
 public class BlueTooth extends Fragment
 {
-    private final String DEVICE_ADDRESS = "00:18:E4:34:CB:D3"; //MAC Address of Bluetooth Module
+    //private final String DEVICE_ADDRESS = "00:18:E4:34:CB:D3"; //MAC Address of Bluetooth Module
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
-    private BluetoothDevice device;
     private BluetoothSocket socket;
     static OutputStream outputStream;
     static InputStream inputStream;
-    Button connect_btn;
-    private ListView devicelistView;
-    private ArrayList devicelist = new ArrayList();
+    private ArrayList<String> deviceList = new ArrayList<>();
+    private ArrayList<String> deviceAddress = new ArrayList<>();
+    private BluetoothAdapter bluetoothAdapter;
 
     public BlueTooth()
     {
@@ -51,10 +48,9 @@ public class BlueTooth extends Fragment
         // Inflate the layout for this fragment
         View BTView = inflater.inflate(R.layout.fragment_blue_tooth, container, false);
 
-        devicelistView = BTView.findViewById(R.id.device_list);
-        connect_btn = BTView.findViewById(R.id.connect_btn);
+        ListView devicelistView = BTView.findViewById(R.id.device_list);
 
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter == null) //Checks if the device supports bluetooth
         {
             Toast.makeText(getActivity(), "Device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
@@ -81,33 +77,30 @@ public class BlueTooth extends Fragment
         {
             for(BluetoothDevice iterator : bondedDevices)
             {
-                devicelist.add(iterator.getName() + "\n" + iterator.getAddress());
-                devicelistView.setAdapter(new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1, devicelist));
-                if(iterator.getAddress().equals(DEVICE_ADDRESS))
-                {
-                    device = iterator;
-                    Toast.makeText(getActivity(),"Found device!", Toast.LENGTH_LONG).show();
-                    break;
-                }
+                deviceList.add(iterator.getName() + "\n" + iterator.getAddress());
+                deviceAddress.add(iterator.getAddress());
+
+                devicelistView.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, deviceList));
             }
         }
-        //Button that connects the app to the robot
-        connect_btn.setOnClickListener(new View.OnClickListener()
+        devicelistView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
-            public void onClick(View v)
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
             {
-                BTconnect();
+                String address = deviceAddress.get(i) + "";
+                BTconnect(address);
             }
         });
 
         return BTView;
     }
-    public boolean BTconnect()
+    public boolean BTconnect(String address)
     {
         boolean connected = true;
         try
         {
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
             socket = device.createRfcommSocketToServiceRecord(PORT_UUID); //Creates a socket to handle the outgoing connection
             socket.connect();
             Toast.makeText(getActivity(),"Connection to bluetooth device successful", Toast.LENGTH_LONG).show();
